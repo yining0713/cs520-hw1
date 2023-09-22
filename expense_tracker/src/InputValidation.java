627bbc479d97ddfa3d00951b8851b5d59219bcc3
Lillian Zhu <lillianzhu@Lillians-MacBook-Pro.local>
2023-09-22 00:37:48 -0400

    add InputValidation class

---
+++ expense_tracker/src/InputValidation.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class InputValidation {
+
+    public static void infoBox(String infoMessage, String titleBar)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void InputValidationAmount(double amount){
+        String amountError = "Please enter a number between 0 and 1000.";
+        if (amount > 1000 || amount < 0) {
+            infoBox(amountError, "Error: Invalid Input");
+        }
+    };
+
+    public static void InputValidationCategory(String category){
+        ArrayList<String> categories = new ArrayList<>();
+        categories.add("food");
+        categories.add("travel");
+        categories.add("bills");
+        categories.add("entertainment");
+        categories.add("other");
+        String categoryError = String.format("Please enter one of the following categories: %s, %s, %s, %s, and %s.", 
+        categories.get(0), categories.get(1), categories.get(2), categories.get(3), categories.get(4));
+        if (!categories.contains(category)){
+            infoBox(categoryError, "Error: Invalid Input");
+        }
+    }
+}
